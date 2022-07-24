c15117e35f1bad8b9a91560ad34ecee168533070
Snap-Skull <104647281+Snap-Skull@users.noreply.github.com>
2022-07-24 16:48:32 -0500

    Create allThreeMiningSimulatorTwoScripts.lua

---
+++ allThreeMiningSimulatorTwoScripts.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Snap-Skull/epicRobloxScript/main/miningSimulatorTwoScript.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Snap-Skull/epicRobloxScript/main/anotherMiningSimulatorTwoScript.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Snap-Skull/epicRobloxScript/main/yepAnotherMiningSimulatorTwoScript.lua", true))()
